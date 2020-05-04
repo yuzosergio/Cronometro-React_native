5210aba620c686c42cb128967471adde14565db6
yuzosergio <yuzosergio@hotmail.com>
2020-05-04 16:58:11 -0300

    cronometro

--- App.js
+++ App.js
@@ -1,19 +1,130 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View,Image,TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      numero: 0,
+      botao: 'INICIAR',
+      ultimo: null
+    };
+
+    //Variavel do timer do relogio
+    this.timer = null;
+
+    this.vai = this.vai.bind(this);
+    this.limpar = this.limpar.bind(this);
+  }
+
+  vai(){
+    if(this.timer != null){
+      //Pausar o timer
+      clearInterval(this.timer);
+      this.timer = null;
+      this.setState({botao: 'INICIAR'})
+    }else{
+
+      //começa girar o timer
+        this.timer = setInterval( ()=> {
+        this.setState({numero: this.state.numero + 0.1})
+
+      }, 100);
+      this.setState({botao: 'PARAR'})
+    }
+  }
+    
+
+  limpar(){
+    if(this.timer != null){
+      clearInterval(this.timer);
+      this.timer = null;
+    }
+    this.setState({
+      ultimo: this.state.numero,
+      numero:0,
+      botao: 'INICIAR'
+    })
+  }
+
+  render(){
+    return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Image
+        source={require('./assets/cronometro.png')}
+        style={styles.cronometro}
+      />
+      
+    <Text style={styles.timer}> {this.state.numero.toFixed(1)} </Text>
+
+      <View style={styles.btnArea}>
+        <TouchableOpacity style={styles.btn} onPress={this.vai}>
+          <Text style={styles.btnTexto}>{this.state.botao}</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.btn} onPress={this.limpar}>
+          <Text style={styles.btnTexto}>Limpar</Text>
+        </TouchableOpacity>
+       
+      </View>
+
+      <View style={styles.areaUltima}>
+          <Text style={styles.textoCorrida}>
+            {this.state.ultimo > 0 ? 'Ultimo tempo: ' + this.state.ultimo.toFixed(2) + 's': ''} 
+          </Text>
+       </View>
     </View>
-  );
+
+    );
+  }
 }
+  
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems:'center',
     justifyContent: 'center',
+    backgroundColor: 'gray',
   },
+  timer:{
+    marginTop:-160,
+    color:'#fff',
+    fontSize: 65,
+    fontWeight: 'bold'
+  },
+  btnArea:{
+    flexDirection: 'row',
+    marginTop:70,
+    height:70
+  },
+  btn:{
+    flex:1,
+    justifyContent:'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    height: 40,
+    margin: 17,
+    borderRadius: 9
+
+  },
+  btnTexto:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'gray'
+  },
+
+  areaUltima:{
+    marginTop: 40,
+  },
+
+  textoCorrida:{
+    fontSize: 25,
+    fontStyle: 'italic',
+    color: '#fff',
+  }
 });
+
+export default App;
